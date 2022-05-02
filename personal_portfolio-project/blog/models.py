e77c9b7d81258fe2a802b448ae832ddecbe8ca57
Baybosunov chyngyz <baybosunov0411@gmail.com>
2022-05-02 21:53:14 +0700

    1
    1

--- personal_portfolio-project/blog/models.py
+++ personal_portfolio-project/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
+    image = models.ImageField(upload_to='portfolio/images/', height_field=None , width_field=None)
     title = models.CharField(max_length=100)
     date  = models.DateField()
     text = models.TextField(max_length=150)
--- personal_portfolio-project/portfolio/models.py
+++ personal_portfolio-project/portfolio/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Project(models.Model):
     image = models.ImageField(upload_to='portfolio/images/', height_field=None , width_field=None)
     title = models.CharField(max_length=100)
-    text = models.CharField(max_length=250)
+    text = models.CharField(max_length=2000)
     url = models.URLField(blank=True)
     
     def __str__(self):
